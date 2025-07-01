55b6ac3e7516c8be0c63e441394d02efe06f1ce8
Alfonso Venegas Purihuaman <avenegas@santaelena.com.pe>
2025-07-01 18:06:28 -0500

    Versión con gráficos, resúmenes y multilenguaje

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ t = texts[lang]
 st.set_page_config(page_title=t["title"], layout="wide")
 st.title(t["title"])
 
-st.image("banner.png", use_column_width=True)
+st.image("banner.png", use_container_width=True)
 
 # Leer datos desde Google Sheets
 sheet_id = "10rYgKcSGsMTq2F2-vh0SxWQNIQ7OVjKS"
